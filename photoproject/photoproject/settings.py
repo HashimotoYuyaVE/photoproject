40b6fce3e59c03628bb4e8cd80c42c8df46d14c8
hashimoto <yuya_hashimoto@totec.co.jp>
2023-05-31 13:45:30 +0900

    7.2(P364まで)

--- photoproject/photoproject/settings.py
+++ photoproject/photoproject/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+# Userモデルの代わりにCustomUserモデルを使用する
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # photoアプリを追加
+    'photo.apps.PhotoConfig',
+    # accountアプリを追加
+    'accounts.app.AccountsConfig',
 ]
 
 MIDDLEWARE = [
